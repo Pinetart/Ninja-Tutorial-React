787c897f6d7d65df56b3ec32cc16198ea01d7e3c
Zane Birkett <axelbirkett@gmail.com>
2022-03-02 10:29:18 -0400

    Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog.";
+  const person = { name: "zane", age: 50 };
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>{person.name}</p>
       </div>
     </div>
   );
