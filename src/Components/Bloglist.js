775576fecbe79c46a0bc166060bb1d3fd742a7d2
Zane Birkett <axelbirkett@gmail.com>
2022-03-03 13:01:42 -0400

    Fetching Data with useEffect

--- src/Components/Bloglist.js
+++ src/Components/Bloglist.js
@@ -1,4 +1,4 @@
-const Bloglist = ({ blogs, title, handleDelete }) => {
+const Bloglist = ({ blogs, title }) => {
   return (
     <div>
       <h2>{title}</h2>
@@ -6,13 +6,6 @@ const Bloglist = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button
-            onClick={() => {
-              handleDelete(blog.id);
-            }}
-          >
-            delete blog
-          </button>
         </div>
       ))}
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,29 +3,22 @@ import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    { title: "Welcome Here!", body: "lorem ipsum...", author: "mario", id: 3 },
-  ]);
-
-  const [name, setName] = useState("mario");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName("Luigi")}>change name</button>
-      <p>{name}</p>
+      {blogs && <Bloglist blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
