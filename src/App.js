22adbfa8ab6f66669d61d91258e8f34cfea70002
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-03-04 10:04:09 -0400

    The React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Components/UI/Navbar";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div>
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
